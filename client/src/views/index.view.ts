27c8be5c4bedbee0c9bd73cebf15a9d4cafb102c
Lenny Claes <lenny.claes@gmail.com>
2019-06-19 22:54:36 +0200

    Added html on index page

--- client/src/views/index.view.ts
+++ client/src/views/index.view.ts
@@ -4,7 +4,17 @@ export class IndexView extends View {
     socket: any;
 
     constructor(socket: Object) {
-        super(`<h1>Homepage</h1>`);
+        super(`
+            <h1>Wie kent Lenny het best?</h1>
+            <h3>Ontdek het nu!</h3>
+            <div class="form-wrap">
+            <form class="form js-form">
+                <label class="form__label" for="name">Jouw naam</label>
+                <input type="text" class="form__input"/>
+                <button type="submit" class="form__submit">IK SPEEL MEE!</button>
+            </form>
+            </div>
+        `);
         this.socket = socket;
     }
 
