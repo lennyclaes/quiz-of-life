16e1205ac0704e4c8352155c0640332ebfbc19ae
Lenny Claes <lenny.claes@gmail.com>
2019-06-20 21:53:53 +0200

    Added player class

---
+++ server/modules/Player.js
@@ -0,0 +1,13 @@
+export class Player {
+
+    constructor(name, socket) {
+        this.name = name;
+        this.score = 0;
+        this.socket = socket;
+    }
+
+    addPoints(points) {
+        this.points += points;
+    }
+
+}
