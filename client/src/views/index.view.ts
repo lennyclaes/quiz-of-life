99fbf6763ac167ff4072288a52509765834e8c60
Lenny Claes <lenny.claes@gmail.com>
2019-06-20 17:55:19 +0200

    Added styling to the window

--- client/src/views/index.view.ts
+++ client/src/views/index.view.ts
@@ -5,14 +5,18 @@ export class IndexView extends View {
 
     constructor(socket: Object) {
         super(`
-            <h1>Wie kent Lenny het best?</h1>
-            <h3>Ontdek het nu!</h3>
-            <div class="form-wrap">
-            <form class="form js-form">
-                <label class="form__label" for="name">Jouw naam</label>
-                <input type="text" class="form__input"/>
-                <button type="submit" class="form__submit">IK SPEEL MEE!</button>
-            </form>
+            <div class="wrap">
+                <h1>Wie kent Lenny het best?</h1>
+                <h3>Ontdek het nu!</h3>
+                <form class="form js-form">
+                    <div class="form-flow">
+                        <label class="form__label" for="name">Jouw naam</label><br>
+                        <input type="text" class="form__input" name="name" />
+                    </div>
+                    <div class="form-flow">
+                        <button type="submit" class="form__submit">IK SPEEL MEE!</button>
+                    </div>
+                </form>
             </div>
         `);
         this.socket = socket;
