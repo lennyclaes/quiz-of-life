e5af6c6bb43f9990d84453c18d77b796570c806f
Lenny Claes <lenny.claes@gmail.com>
2019-06-20 19:08:09 +0200

    Added styling to join screen

--- client/src/views/index.view.ts
+++ client/src/views/index.view.ts
@@ -7,11 +7,11 @@ export class IndexView extends View {
         super(`
             <div class="wrap">
                 <h1 class="title" >Wie kent Lenny het best?</h1>
-                <h3>Ontdek het nu!</h3>
+                <h3 class="title-sub">Ontdek het nu!</h3>
                 <form class="form js-form">
                     <div class="form-flow">
                         <label class="form__label" for="name">Jouw naam</label><br>
-                        <input type="text" class="form__input" name="name" />
+                        <input type="text" class="form__input" name="name" autocomplete="off" />
                     </div>
                     <div class="form-flow">
                         <button type="submit" class="form__submit">IK SPEEL MEE!</button>
